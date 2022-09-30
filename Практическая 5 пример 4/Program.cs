3fe5d33a3b4a082ba55c7be0a7030f540bbada83
barinskaya <1388zxc@gmail.com>
2022-09-30 15:29:00 +0300

    Добавьте файлы проекта.

---
+++ Практическая 5 пример 4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Практическая_5_пример_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
